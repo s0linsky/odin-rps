daef55ee8e15cadc959dc682ec3e96080861d0ce
s0linsky <solinskybruh@gmail.com>
2022-11-22 16:52:36 -0500

    Add comments explaining code

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// A function that returns an integer between the minimum of 1 and the defined maximum
 function getComputerChoice() {
   function getRandomInt(max) {
     return Math.floor(Math.random() * max) + 1;
   }
 
+  // Return a random integer between 1 and 3
+  // Depending on the integer returned, log the corresponding string
   let randomInt = getRandomInt(3);
 
   if (randomInt === 1) {
