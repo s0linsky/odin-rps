d940a3a9d6b7e6f70d910eec6037cfb6edcbfcbe
s0linsky <solinskybruh@gmail.com>
2022-11-29 16:12:40 -0500

    Add comments

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function getComputerChoice() {
   }
 }
 
-// Play a single round between the players choice and the computers choice
+// Play a single round between the player's choice and the computer's choice
 // Return the results of the round
 function playRound(cpuChoice) {
   let userChoice = prompt("Rock, Paper or Scissors?");
@@ -54,31 +54,47 @@ function playRound(cpuChoice) {
 }
 
 // Play a game consisting of 5 single rounds
-// If there is a draw, move on to the next round
 // After 5 rounds, the most victories wins the game
+// If there is a draw, move on to the next round
 // After each round, log the winner of the round
 // After all rounds are complete, log the winner of the games
 function game() {
   let playerScore = 0;
   let cpuScore = 0;
+  let drawCount = 0;
 
   for (let i = 0; i < 5; i++) {
     result = playRound(getComputerChoice());
     console.log(result);
     if (result.includes("player wins")) {
       playerScore++;
-      console.log("Player Score: " + playerScore);
     } else if (result.includes("CPU wins")) {
       cpuScore++;
-      console.log("CPU Score: " + cpuScore);
+    } else if (result.includes("Draw")) {
+      drawCount++;
     }
   }
 
   if (playerScore > cpuScore) {
+    console.log(
+      "CPU Score: " + cpuScore + " || " + "Player Score: " + playerScore
+    );
     console.log("Player Wins The Game!");
   } else if (cpuScore > playerScore) {
+    console.log(
+      "Player Score: " + playerScore + " || " + "CPU Score: " + cpuScore
+    );
     console.log("CPU Wins The Game.");
   } else if (playerScore >= 1 && playerScore === cpuScore) {
+    console.log(
+      "Player Score: " +
+        playerScore +
+        " || " +
+        "CPU Score: " +
+        cpuScore +
+        " ||  Number of Draws: " +
+        drawCount
+    );
     console.log("Tie game! Play again.");
   } else console.log("Something went wrong. Play again.");
 }
